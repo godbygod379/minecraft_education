f257c4828063eac98db61fae2ad6a203ce2d962c
godbygod379 <godbygod379@gmail.com>
2025-12-04 17:27:29 +0100

    piramid:teleport player and slot change

--- piramid.py
+++ piramid.py
@@ -1,3 +1,9 @@
 def on_on_chat():
-    for square
+    agent.teleport_to_player()
+    agent.set_slot(1)
+    for square in range(4):
+        for side in range(9):
+            agent.place(DOWN)
+            agent.move(FORWARD, 1)
+        agent.turn(LEFT_TURN)
 player.on_chat("run", on_on_chat)
