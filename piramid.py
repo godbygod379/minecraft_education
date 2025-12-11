4064d4714d072d000d0154a07e87d612e2123578
godbygod379 <godbygod379@gmail.com>
2025-12-11 16:46:59 +0100

    Prva funkcna verzia kodu pyramidy

--- piramid.py
+++ piramid.py
@@ -1,9 +1,14 @@
-def on_on_chat():
-    agent.teleport_to_player()
+def on_on_chat(hi):
+    agent.teleport(pos(0, 10, 0), WEST)
     agent.set_slot(1)
-    for square in range(4):
-        for side in range(9):
-            agent.place(DOWN)
-            agent.move(FORWARD, 1)
-        agent.turn(LEFT_TURN)
+    while hi > 0:
+        for square in range(4):
+            for side in range(0, hi):
+                agent.place(DOWN)
+                agent.move(FORWARD, 1)
+            agent.turn(LEFT_TURN)
+        agent.move(LEFT, 1)
+        agent.move(UP, 1)
+        agent.move(FORWARD, 1)
+        hi = hi - 2
 player.on_chat("run", on_on_chat)
