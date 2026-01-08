ee5c83c125ea41e28b3c8a44c12cfbeb96c2c821
godbygod379 <godbygod379@gmail.com>
2026-01-08 17:39:37 +0100

    Update check.py

--- checker.py
+++ checker.py
@@ -2,17 +2,22 @@ def on_on_chat():
     agent.teleport_to_player()
 
     for chessboard in range(4):
-        line1()
+        odd_line()
         agent.turn(RIGHT_TURN)
         agent.move(FORWARD, 1)
         agent.turn(RIGHT_TURN)
-        line2()
+        even_line()
         agent.turn(LEFT_TURN)
         agent.move(FORWARD, 1)
         agent.turn(LEFT_TURN)
+    agent.turn(LEFT_TURN)
+    agent.move(FORWARD, 1)
+    agent.turn(RIGHT_TURN)
+    agent.move(UP, 1)
 
-def line1():
-    for line3 in range(4):
+
+def odd_line():
+    for i in range(4):
         agent.move(FORWARD, 1)
         agent.set_slot(1)
         agent.place(DOWN)
@@ -20,13 +25,20 @@ def line1():
         agent.set_slot(2)
         agent.place(DOWN)
 
-def line2():
-    for line4 in range(4):
+def even_line():
+    for i in range(4):
         agent.set_slot(1)
         agent.place(DOWN)
         agent.move(FORWARD, 1)
         agent.set_slot(2)
         agent.place(DOWN)
         agent.move(FORWARD, 1)
+
+def figure_odd_line():
+    agent.move(FORWARD, 1)
+    agent.set_slot(3)
+    agent.place(DOWN)
+    agent.move(FORWARD, 2)
+    agent.place(DOWN)
 
 player.on_chat("run", on_on_chat)
