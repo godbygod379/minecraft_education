c9225f04d7af74bb3859a75613fded4ed0d4f713
godbygod379 <godbygod379@gmail.com>
2026-01-08 16:47:43 +0100

    adding_function_line2.py

--- checker.py
+++ checker.py
@@ -1,7 +1,17 @@
 def on_on_chat():
     agent.teleport_to_player()
-    def line1():
-        
+
+    for chessboard in range(4):
+        line1()
+        agent.turn(RIGHT_TURN)
+        agent.move(FORWARD, 1)
+        agent.turn(RIGHT_TURN)
+        line2()
+        agent.turn(LEFT_TURN)
+        agent.move(FORWARD, 1)
+        agent.turn(LEFT_TURN)
+
+def line1():
     for line3 in range(4):
         agent.move(FORWARD, 1)
         agent.set_slot(1)
@@ -9,4 +19,14 @@ def on_on_chat():
         agent.move(FORWARD, 1)
         agent.set_slot(2)
         agent.place(DOWN)
+
+def line2():
+    for line4 in range(4):
+        agent.set_slot(1)
+        agent.place(DOWN)
+        agent.move(FORWARD, 1)
+        agent.set_slot(2)
+        agent.place(DOWN)
+        agent.move(FORWARD, 1)
+
 player.on_chat("run", on_on_chat)
